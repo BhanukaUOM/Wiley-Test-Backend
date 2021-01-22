49087fd646f4aee0a799562963ee5904ffedde84
Sadheera Mahanama <sadheeramahanama94@gmail.com>
2021-01-22 18:56:21 +0530

    Init project

---
+++ src/main/java/com/sadheera/wileytest/WileyTestApplication.java
@@ -0,0 +1,13 @@
+package com.sadheera.wileytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WileyTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WileyTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sadheera/wileytest/WileyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sadheera.wileytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WileyTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
